7641f84291791245e82e8d29eb3f8b441c2813cf
Arnax15 <104311416+Arnax15@users.noreply.github.com>
2024-08-12 12:11:38 +0530

    Create Student.java

---
+++ Week2_Collections/Week2_Ex8/Student.java
@@ -0,0 +1,23 @@
+public class Student {
+    private int id;
+    private String name;
+    private char grade;
+
+    public Student(int id, String name, char grade) {
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public char getGrade() {
+        return grade;
+    }
+}
