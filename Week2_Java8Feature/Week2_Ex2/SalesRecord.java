4d4d82d00357781581955b15957270be75d7659b
Arnax15 <104311416+Arnax15@users.noreply.github.com>
2024-08-12 12:27:25 +0530

    Create SalesRecord.java

---
+++ Week2_Java8Feature/Week2_Ex2/SalesRecord.java
@@ -0,0 +1,9 @@
+public class SalesRecord {
+    private int recordId;
+    private String salesPerson;
+    private String region;
+    private double amount;
+    private Date date;
+
+    // getters and setters
+}
